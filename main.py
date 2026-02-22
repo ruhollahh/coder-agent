aa599951f649b103fc1ef2af566c0e3610f3c8f1
ruhollahh <ruhollahh01@gmail.com>
2026-02-23 01:26:31 +0330

    init

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+import argparse
+
+# from google import genai
+import cerebras.cloud.sdk as cerebrassdk
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    load_dotenv()
+    api_key = os.environ.get("CEREBRAS_API_KEY")
+    if api_key == None:
+        raise RuntimeError("no api key provided in .env file")
+    # client = genai.Client(api_key=api_key)
+    # response = client.models.generate_content(model="gemini-2.5-flash", contents="")
+    # response.text
+    client = cerebrassdk.Cerebras(api_key=api_key)
+
+    user_prompt = args.user_prompt
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_prompt,
+                }
+            ],
+            model="gpt-oss-120b",
+        )
+    except cerebrassdk.APIConnectionError as e:
+        print("The server could not be reached")
+        print(e.__cause__)  # an underlying Exception, likely raised within httpx.
+    except cerebrassdk.RateLimitError as e:
+        print("A 429 status code was received; we should back off a bit.")
+    except cerebrassdk.APIStatusError as e:
+        print("Another non-200-range status code was received")
+        print(e.status_code)
+        print(e.response)
+    except Exception as e:
+        print(f"failed to create chat completion: {e}")
+
+    if (
+        len(chat_completion.choices) < 1
+        or chat_completion.choices[0].finish_reason != "stop"
+    ):
+        raise RuntimeError(f"something went wrong: {chat_completion}")
+
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {chat_completion.usage.prompt_tokens}")
+    print(f"Response tokens: {chat_completion.usage.completion_tokens}")
+    print(f"Response:\n{chat_completion.choices[0].message.content}")
+
+
+if __name__ == "__main__":
+    main()
